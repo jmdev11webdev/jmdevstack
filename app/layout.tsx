a3bf1b3db4da3dd03d586bc7c93f0931b2f6ce7a
jmdev11webdev <jmdev.11.fsd@gmail.com>
2026-01-10 13:05:24 +0800

    installed analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/next"
 
 export const metadata: Metadata = {
   title: "JMDevStack",
@@ -35,6 +36,7 @@ export default function RootLayout({
 
       <body>
         {children}
+        <Analytics />
       </body>
     </html>
   );
